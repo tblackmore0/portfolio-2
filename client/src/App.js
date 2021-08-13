32f400453062abae4a6f8a1dfa6fd826185aa280
Tom <tblackmore0@gmail.com>
2021-08-13 20:00:19 +0100

    Testing new structure

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+/* import { NavBar } from './Components/NavBar';
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+import { Projects } from './Components/Projects';
+import { Copyright } from './Components/Copyright'; */
+
+import { Contact } from './Components/Contact';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
+
