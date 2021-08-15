4e551d63115e7a42d2f6913e55060a18e119ebcd
Tom <tblackmore0@gmail.com>
2021-08-15 14:33:08 +0100

    testing heroku push

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,25 @@ import { Copyright } from './Components/Copyright'; */
 
 import { Contact } from './Components/Contact';
 
+/* const path = require('path');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+*/
 
 function App() {
 
--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-//app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-//app.get('*', (req, res) => {
-//  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
-
+console.log('fuck this');
 // TRANSPORTER
 
 let transport = {
